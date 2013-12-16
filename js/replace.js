123a5ed50b21d583bc0a2fa41fc08a3f59f09017
Kasper Lind Sørensen <omglolkkthxbye@gmail.com>
2013-12-16 19:59:49 +0100

    Moved the conversion of input to regexp to convertToRegExp function.

--- js/replace.js
+++ js/replace.js
@@ -3,12 +3,7 @@ chrome.storage.sync.get("status", function(result){
 		$(function() {
 			var regularExpressions = [];
 			chrome.storage.sync.get(null,function(items){
-				for (var item in items){
-					if (regularExpressions.indexOf(item) == -1){
-						regularExpressions.push(new RegExp(item, "i"));
-					}
-					
-				}
+				convertToRegExp(items, regularExpressions);
 				var iterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT);
 				while (node = iterator.nextNode()) {
 					replace(node, regularExpressions);
@@ -23,5 +18,13 @@ chrome.storage.sync.get("status", function(result){
 function replace(node, regularExpressions) {
 	for (var expression in regularExpressions){
 		node.data = node.data.replace(regularExpressions[expression], 'Ding Dong');
+	}
+}
+
+function convertToRegExp(items, targetArray){
+	for (var item in items){
+		if (targetArray.indexOf(item) == -1){
+			targetArray.push(new RegExp(item, "i"));
+		}
 	}
 }
