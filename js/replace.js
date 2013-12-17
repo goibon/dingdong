272ab7762cc456ffa1ccdd0857f89560ddf407a2
Kasper Lind Sørensen <omglolkkthxbye@gmail.com>
2013-12-17 17:21:37 +0100

    Added listener to update whenever storage is changed.

--- js/replace.js
+++ js/replace.js
@@ -1,22 +1,30 @@
+chrome.storage.onChanged.addListener(function(changes, namespace){
+	chrome.storage.sync.get("status", function(result){
+		if (result["status"] == "enabled"){
+			replace();
+		}
+	});
+});
+
 chrome.storage.sync.get("status", function(result){
 	if (result["status"] == "enabled"){
-		$(function() {
-			var regularExpressions = [];
-			chrome.storage.sync.get(null,function(items){
-				convertToRegExp(items, regularExpressions);
-				var iterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT);
-				while (node = iterator.nextNode()) {
-					replace(node, regularExpressions);
-				}
-			});
-		});
+		replace();
 	}
 });
 
-function replace(node, regularExpressions) {
-	for (var expression in regularExpressions){
-		node.data = node.data.replace(regularExpressions[expression], 'Ding Dong');
-	}
+function replace() {
+  $(function() {
+    var regularExpressions = [];
+  	chrome.storage.sync.get(null,function(items){
+  		convertToRegExp(items, regularExpressions);
+  		var iterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT);
+  		while (node = iterator.nextNode()) {
+  			for (var expression in regularExpressions){
+          node.data = node.data.replace(regularExpressions[expression], 'Ding Dong');
+        }
+  		}
+  	});
+  });
 }
 
 function convertToRegExp(items, targetArray){
